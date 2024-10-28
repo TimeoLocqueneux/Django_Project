78790c5fdfaf823d388eb043f76d1db468b403b8
TitouanBodin <titouan2gode@gmail.com>
2024-10-28 18:01:53 +0100

    Frontend + test affichage

--- drive/myapp/views.py
+++ drive/myapp/views.py
@@ -1,5 +1,6 @@
 import os
 from django.shortcuts import render, HttpResponse
+from datetime import datetime
 
 # Create your views here.
 def home(request):
@@ -12,13 +13,24 @@ def main(request):
     # Obtenir le chemin absolu du répertoire source du projet
     project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
     
-    # Naviguer dans le sous-dossier 'myapp'
+    # Naviguer dans le sous-dossier 'myapp/static/img'
     images_pour_site = os.path.join(project_root, 'myapp', 'static', 'img')
     
-    # Lister les fichiers et les dossiers dans le sous-dossier 'myapp'
-    files = os.listdir(images_pour_site)
+    # Lister les fichiers et les dossiers dans le sous-dossier 'myapp/static/img'
+    files_info = []
+    for file_name in os.listdir(images_pour_site):
+        file_path = os.path.join(images_pour_site, file_name)
+        if os.path.isfile(file_path):
+            file_size = os.path.getsize(file_path)
+            file_mtime = os.path.getmtime(file_path)
+            file_mtime = datetime.fromtimestamp(file_mtime).strftime('%Y-%m-%d')
+            files_info.append({
+                'name': file_name,
+                'size': (file_size/1000).__int__(),
+                'mtime': file_mtime
+            })
     
-    return render(request, 'main.html', {'files': files, 'directory': images_pour_site})
+    return render(request, 'main.html', {'files': files_info, 'directory': images_pour_site})
 
 def profile(request):
     return render(request, "profile.html")
