092e8e3e7b639cba91f3ac84baa7ab5d7185a2ed
Sacha-Vandemeulebroucke <145006455+Sacha-Vandemeulebroucke@users.noreply.github.com>
2024-10-29 18:27:28 +0100

    changement de page d'accueil: main -> login connexion data base avec register mais erreur avec le login(il n'y a pas encore de lien entre la database et le formulaire de connexion qui vérifie si le user entre le bon mail avec le bon password

--- drive/myapp/urls.py
+++ drive/myapp/urls.py
@@ -2,12 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.main, name="main"),
+    path("", views.login, name="login"),
     path("main/", views.main, name="main"),
     path("home/",views.home, name="home"),
-    path("login/", views.login, name='login'),
     path("profile/", views.profile, name='profile'),
     path('success/', views.success_view, name='success'),
     path('register/', views.register_view, name='register'),
-    path('login_view/', views.login_view, name='login')
-]
+    ]
