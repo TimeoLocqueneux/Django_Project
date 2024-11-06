a2a1506a85cfc14c82a068370da9aa6324584e32
imreaallybad <timeo62000@gmail.com>
2024-11-06 18:07:50 +0100

    move files and folders

--- drive/myapp/views.py
+++ drive/myapp/views.py
@@ -8,6 +8,7 @@ from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse, JsonResponse
 import json
 import logging
+import shutil
 
 base_dir = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'uploads')
 
@@ -325,20 +326,25 @@ def download_file(request):
 
 
 def move_file(request):
+    print('move')
     if request.method == 'POST':
         body = json.loads(request.body)
         file_name = body.get('file_name')
         folder_name = body.get('folder_name')
         path = body.get('path', '')
+        print(file_name, folder_name, path)
         current_dir = os.path.join(base_dir, path)
         file_path = os.path.join(current_dir, file_name)
         new_folder_path = os.path.join(current_dir, folder_name)
         new_file_path = os.path.join(new_folder_path, file_name)
-        print(file_path, new_file_path, new_folder_path)
+        print(file_path, new_folder_path, new_file_path)
         if os.path.exists(file_path) and os.path.exists(new_folder_path):
-            os.rename(file_path, new_file_path)
-            return render(request, 'main.html', {'path': folder_name})
-        
+            if os.path.isdir(file_path):
+                print("is dir")
+                shutil.move(file_path, new_folder_path)
+            else:
+                shutil.move(file_path, new_file_path)
+            return redirect('main_with_path', path=folder_name)
         else:
             error_message = "Fichier ou dossier introuvable."
             return render(request, 'main.html', {'error': error_message, 'path': path})
